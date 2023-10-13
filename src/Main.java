b896edc19dde0ee1e200c03edbdbafef4413ffa5
Alex <alex.shalimovlol@gmail.com>
2023-10-13 15:35:29 +0300

    Fixed time.start and player.start.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
+    static boolean isWin = false;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int random = (int) (Math.random() * 1_000_000_000);
+        System.out.println(random);
+        Thread timer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                try {
+                    while (!isWin) {
+                        System.out.println(i);
+                        i++;
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread player = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!isWin) {
+                    int guessNumber = (int) (Math.random() * 1_000_000_000);
+                    if (guessNumber == random) {
+                        isWin = true;
+                        System.out.println(guessNumber);
+                    }
+                }
+            }
+        });
+        timer.start();
+        player.start();
     }
 }
